06bb9949cba23f4798af02c7828a19cb6ea8ffce
Piplsingha <pipolchandrasingha@gmail.com>
2022-12-12 23:03:33 +0600

    first commit

---
+++ js/style.js
@@ -0,0 +1,14 @@
+var form =document.querySelector("form");
+document.querySelector("#log_in").click = () =>{
+    form.classList.toggle("active");
+}
+
+
+
+
+
+
+const btn = document.querySelector(".button");
+
+btn.classList.add("button--loading");
+btn.classList.contains("button--loading");
